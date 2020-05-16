447061484c1623bbf6eed121693c67b9e91805f6
crettin <nicholas_harrison@live.co.uk>
2020-05-16 16:23:00 +0100

    Database changes - added account creation and login logic.
    Database changes - added account creation and login logic.

--- server/zone-manager.js
+++ server/zone-manager.js
@@ -1,6 +1,6 @@
 const roomManager = require('./room-manager.js');
 const systems = require('./systems.js');
-const characters = require('./server_characters.js');
+const characters = require('./characters.js');
 systems.startUpdate();
 
 
